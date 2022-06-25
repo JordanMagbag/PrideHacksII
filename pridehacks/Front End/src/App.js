6c6511e76bde940adaa1c6c0e6709792a16bac6b
juan-rios13 <80425878+juan-rios13@users.noreply.github.com>
2022-06-25 18:22:20 -0500

    home page update

--- pridehacks/Front End/src/App.js
+++ pridehacks/Front End/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from "react";
-import Login from "./components/Login"
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Home/>
     </div>
   );
 }
--- pridehacks/Front End/src/components/Home.js
+++ pridehacks/Front End/src/components/Home.js
@@ -1,7 +1,19 @@
 import "./Home.css"
+import React from 'react';
 
 const Home=()=>{
+    return(
+        <><body>
+            </body>
+        <div class="navbar">
+            <a href="#home" class="active">Option 1</a>
+            <a href="#news">Option 2</a>
+            <a href="#contact">Option 3</a>
+            <a href="#option4">Option 4</a>
+            </div>
+        </>
+    );
 
-}
+};
 
 export default Home;
