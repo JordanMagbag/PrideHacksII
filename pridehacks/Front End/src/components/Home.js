825dfd4bad1cf624a0a97d4a3b4024ac1e67bc27
Samantha Tsai <samanthatsai@Samanthas-MacBook-Pro.local>
2022-06-25 19:33:31 -0500

    navbar

--- pridehacks/Front End/src/components/Home.js
+++ pridehacks/Front End/src/components/Home.js
@@ -1,17 +1,18 @@
 import "./Home.css"
 import React from 'react';
+import background from "/Users/samanthatsai/Downloads/PrideHacks/PrideHacksII/pridehacks/Front End/src/components/map.png";
+import tp from "/Users/samanthatsai/Downloads/PrideHacks/PrideHacksII/pridehacks/Front End/src/images/toilet hack 2.png"
 
 const Home=()=>{
     return(
-        <><body>
-            </body>
-        <div class="navbar">
-            <a href="#home" class="active">Option 1</a>
-            <a href="#news">Option 2</a>
-            <a href="#contact">Option 3</a>
-            <a href="#option4">Option 4</a>
+        <body>
+            <img src={background} alt="map" id = "map"></img>
+            <div class="navbar">
+                <a href="#home"><img src = {tp} alt = "3 Maps Logo" width = "114px" height = "107px" border = "1px solid black" ></img></a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
             </div>
-        </>
+        </body>
     );
 
 };
